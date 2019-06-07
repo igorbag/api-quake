a9fb2294d8ed4c48be8d64e73ccabadc3adf8b80
feodpv00933 <igor.bagliotti>
2019-06-07 16:18:36 -0300

    Adicionando Constantes identificadas para utilização

--- src/main/java/br/com/quake/parser/api/data/local/Constants.java
+++ src/main/java/br/com/quake/parser/api/data/local/Constants.java
@@ -2,4 +2,12 @@ package br.com.quake.parser.api.data.local;
 
 public final class Constants {
     public static final String GAMES_LOG_DATA_NAME = "games.log";
+    public static final String INIT_GAME = "InitGame";
+    public static final String CLIENT_USER_INFO_CHANGED = "ClientUserinfoChanged";
+    public static final String N_DELIMITER = "n" + "\\";
+    public static final String T_DELIMITER = "\\" + "t";
+    public static final String BY_DELIMITER = "by";
+    public static final String KILL_PATTERN = "Kill";
+    public static final String KILLED_PATTERN = " killed";
+    public static final String GAME_NUMBER = "game_";
 }
