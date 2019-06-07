65b89063c9ba8632a092445cdc481063076cbb42
feodpv00933 <igor.bagliotti>
2019-06-07 15:03:01 -0300

    Adicionando constante responsavel pelo nome do arquivo

--- src/main/java/br/com/quake/parser/api/data/local/Constants.java
+++ src/main/java/br/com/quake/parser/api/data/local/Constants.java
@@ -1,5 +1,5 @@
 package br.com.quake.parser.api.data.local;
 
 public final class Constants {
-    public static final double PLANCK_CONSTANT = 6.62606896e-34;
+    public static final String GAMES_LOG_DATA_NAME = "games.log";
 }
--- src/main/java/br/com/quake/parser/api/service/QuakeInfoService.java
+++ src/main/java/br/com/quake/parser/api/service/QuakeInfoService.java
@@ -1,5 +1,6 @@
 package br.com.quake.parser.api.service;
 
+import br.com.quake.parser.api.data.local.Constants;
 import io.reactivex.Flowable;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ public class QuakeInfoService {
 
     private Flowable getGamesLog() {
         return Flowable.using(
-                () -> new BufferedReader(new FileReader(new ClassPathResource("games.log").getFile())),
+                () -> new BufferedReader(new FileReader(new ClassPathResource(Constants.GAMES_LOG_DATA_NAME).getFile())),
                 reader -> Flowable.fromIterable(() -> reader.lines().iterator()),
                 reader -> reader.close()
         );
