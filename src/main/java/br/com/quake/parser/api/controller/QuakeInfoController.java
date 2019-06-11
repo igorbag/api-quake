44f221d373c67c77bce3e4572e56ad787944124f
feodpv00933 <igor.bagliotti>
2019-06-10 22:34:17 -0300

    Integrando o retorno de resultados controller / Service

--- src/main/java/br/com/quake/parser/api/controller/QuakeInfoController.java
+++ src/main/java/br/com/quake/parser/api/controller/QuakeInfoController.java
@@ -1,11 +1,13 @@
 package br.com.quake.parser.api.controller;
 
+import br.com.quake.parser.api.model.Game;
 import br.com.quake.parser.api.service.QuakeInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/game")
@@ -15,11 +17,7 @@ public class QuakeInfoController {
     private QuakeInfoService quakeInfoService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity quakeInfo() {
-
-        quakeInfoService.process();
-
-        return ResponseEntity.badRequest()
-                .body("Olá Quake Api");
+    public Map<String, Game> quakeInfo() {
+        return quakeInfoService.process();
     }
 }
