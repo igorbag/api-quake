1558444c03c1cc731d791eecc35991e2d7027cc0
feodpv00933 <igor.bagliotti>
2019-06-11 17:30:44 -0300

    Adicionando testes no quake info service

--- src/test/java/br/com/quake/parser/api/QuakeInfoServiceTest.java
+++ src/test/java/br/com/quake/parser/api/QuakeInfoServiceTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,5 +22,21 @@ public class QuakeInfoServiceTest {
     @Test
     public void getGameInfoTest() {
         Assert.assertTrue(quakeInfoService.getGameInfo().size() > 0);
+    }
+
+    @Test
+    public void getGameInfoByIdTest() {
+        int id = 1;
+        Assert.assertNotNull(quakeInfoService.getGameInfoById(id));
+    }
+
+
+    @Test
+    public void setUpGameInformationsTest() {
+        final List<String> gamesMatch = new ArrayList<>();
+        final String teste = "0:00 ClientUserinfoChanged: 3 n\\Dono da Bola\\t\\3\\model\\sarge\\hmodel\\sarge\\g_redteam\\\\g_blueteam\\\\c1\\4\\c2\\5\\hc\\95\\w\\0\\l\\0\\tt\\0\\tl\\0";
+        gamesMatch.add(teste);
+
+        Assert.assertNotNull(quakeInfoService.setUpGameInformations(gamesMatch));
     }
 }
