2f2048b47d3359badbcb6e656f7a3c7eb2a9e040
feodpv00933 <igor.bagliotti>
2019-06-10 22:28:41 -0300

    Refatorando o player Name service

--- src/main/java/br/com/quake/parser/api/service/PlayerNameService.java
+++ src/main/java/br/com/quake/parser/api/service/PlayerNameService.java
@@ -2,20 +2,25 @@ package br.com.quake.parser.api.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static br.com.quake.parser.api.data.local.Constants.*;
 
 @Service
 public class PlayerNameService {
 
-    public String getPlayerName(final String itemGameLog) {
-
-        String playerName = "";
-
-        if (itemGameLog.contains(CLIENT_USER_INFO)) {
-            playerName = itemGameLog.substring(itemGameLog.indexOf(N_DELIMITER) + 2, itemGameLog.indexOf(T_DELIMITER));
+    public List<String> getPlayersName(final List<String> listItemGame) {
+        List<String> players = new ArrayList();
+        for (String itemGame : listItemGame) {
+            if (itemGame.contains(CLIENT_USER_INFO)) {
+                String userName = itemGame.substring(itemGame.indexOf(N_DELIMITER) + 2, itemGame.indexOf(T_DELIMITER));
+                if (!players.contains(userName)) {
+                    players.add(userName);
+                }
+            }
         }
-
-        return playerName;
+        return players;
     }
 
 }
