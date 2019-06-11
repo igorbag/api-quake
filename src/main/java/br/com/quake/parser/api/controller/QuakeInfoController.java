1e39264b007cc65574a957f916b3dced643e38e9
feodpv00933 <igor.bagliotti>
2019-06-11 11:56:48 -0300

    Colocando response Entity no retorno da API

--- src/main/java/br/com/quake/parser/api/controller/QuakeInfoController.java
+++ src/main/java/br/com/quake/parser/api/controller/QuakeInfoController.java
@@ -3,6 +3,7 @@ package br.com.quake.parser.api.controller;
 import br.com.quake.parser.api.model.Game;
 import br.com.quake.parser.api.service.QuakeInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,12 +19,12 @@ public class QuakeInfoController {
     private QuakeInfoService quakeInfoService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public Map<String, Game> getGameInfo() {
-        return quakeInfoService.getGameInfo();
+    public ResponseEntity<Map<String, Game>> getGameInfo() {
+        return ResponseEntity.ok(quakeInfoService.getGameInfo());
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    public Game quakeInfoById(@PathVariable Integer id) {
-        return quakeInfoService.getGameInfoById(id);
+    public ResponseEntity<Game> quakeInfoById(@PathVariable Integer id) {
+        return ResponseEntity.ok(quakeInfoService.getGameInfoById(id));
     }
 }
