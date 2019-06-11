341a9d7b406332fbf79b5f3d1ac5e79b986dfb75
feodpv00933 <igor.bagliotti>
2019-06-10 22:21:23 -0300

    Adicionando constantes do jogo

--- src/main/java/br/com/quake/parser/api/data/local/Constants.java
+++ src/main/java/br/com/quake/parser/api/data/local/Constants.java
@@ -4,10 +4,13 @@ public final class Constants {
     public static final String GAMES_LOG_DATA_NAME = "games.log";
     public static final String INIT_GAME = "InitGame";
     public static final String CLIENT_USER_INFO = "ClientUserinfoChanged";
+    public static final String END_GAME = "ShutdownGame";
     public static final String N_DELIMITER = "n" + "\\";
     public static final String T_DELIMITER = "\\" + "t";
     public static final String BY_DELIMITER = "by";
     public static final String KILL_PATTERN = "Kill";
     public static final String KILLED_PATTERN = " killed";
-    public static final String GAME_NUMBER = "game_";
+    public static final String GAME = "game_";
+    public static final String WORLD = "world";
+
 }
