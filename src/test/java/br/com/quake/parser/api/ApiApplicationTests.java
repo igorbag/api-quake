dcc01ee9c2543ff0fe04f0cbcbe1fb6cb9afa1ac
feodpv00933 <igor.bagliotti>
2019-06-11 21:54:44 -0300

    Adicionando classes na suite de testes

--- src/test/java/br/com/quake/parser/api/ApiApplicationTests.java
+++ src/test/java/br/com/quake/parser/api/ApiApplicationTests.java
@@ -9,7 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Suite.SuiteClasses({
-        QuakeInfoServiceTest.class
+        QuakeInfoServiceTest.class,
+        KillGameServiceTest.class,
+        PlayerNameServiceTest.class,
+        GameServiceTest.class
 })
 public class ApiApplicationTests {
 
